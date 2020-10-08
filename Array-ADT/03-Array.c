65e5bd830e6a8963803384b0d342317929abd8c7
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-08 18:49:11 +0530

    creating 04-array.c

--- Array-ADT/03-Array.c
+++ Array-ADT/03-Array.c
@@ -27,12 +27,13 @@ void Insert(struct Array *arr, int index, int val)
     arr->length++;
   }
 }
+
 int main()
 {
   //Initializing array so that we won't have to input everytime just to test the program
   struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
 
-  Append(&arr, 10);
+  Insert(&arr, 5, 10);
   Display(arr);
 
   return 0;
---
+++ Array-ADT/04-Array.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void Delete(struct Arr *arr, int index)
+{
+}
+
+int main()
+{
+
+  return 0;
+}
