9b9f7189952a9938646841806ae19e41ce38df29
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-08 23:43:04 +0530

    Added isSorted

--- Array-ADT/09-Array.c
+++ Array-ADT/09-Array.c
@@ -15,12 +15,22 @@ void Insert_in_Sorted(Array *arr, int val)
   arr->length++;
 }
 
+int isSorted(Array arr)
+{
+  for (int i = 0; i < arr.length - 1; i++)
+  {
+    if (arr.arr[i] > arr.arr[i + 1])
+      return 0;
+  }
+  return 1;
+}
+
 int main()
 {
   Array arr = {{2, 3, 5, 10, 15}, 10, 5};
 
   Insert_in_Sorted(&arr, 12);
-
+  printf("%d\n", isSorted(arr));
   Display(arr);
   return 0;
 }
