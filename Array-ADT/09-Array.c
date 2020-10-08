68175a4388c1eaf18c3f105b6b45d5bc640ca5bf
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-08 23:39:26 +0530

    insert in a sorted array done

--- Array-ADT/09-Array.c
+++ Array-ADT/09-Array.c
@@ -1,13 +1,25 @@
 #include <stdio.h>
 #include "Array.h"
 
-void Insert_in_Sorted()
+void Insert_in_Sorted(Array *arr, int val)
 {
+  int i = arr->length - 1;
+  if (arr->length == arr->size)
+    return;
+  while (i >= 0 && arr->arr[i] > val)
+  {
+    arr->arr[i + 1] = arr->arr[i];
+    i--;
+  }
+  arr->arr[i + 1] = val;
+  arr->length++;
 }
 
 int main()
 {
   Array arr = {{2, 3, 5, 10, 15}, 10, 5};
+
+  Insert_in_Sorted(&arr, 12);
 
   Display(arr);
   return 0;
