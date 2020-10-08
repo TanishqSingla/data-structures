a5da8862b37f073fddd52a42c6d46b8e5b7cc39a
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-08 18:51:09 +0530

    Creating a header for array

--- Array-ADT/04-Array.c
+++ Array-ADT/04-Array.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <Array.h>
 
 void Delete(struct Arr *arr, int index)
 {
@@ -6,6 +7,11 @@ void Delete(struct Arr *arr, int index)
 
 int main()
 {
+  //Initializing array so that we won't have to input everytime just to test the program
+  struct Array arr = {{2, 3, 4, 5, 6}, 20, 5};
+
+  Insert(&arr, 5, 10);
+  Display(arr);
 
   return 0;
 }
---
+++ Array-ADT/Array.h
@@ -0,0 +1,7 @@
+struct Array
+{
+  //setting fixed size just for implementations
+  int arr[20];
+  int size;
+  int length;
+};
