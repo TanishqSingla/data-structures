d8e4e1f844741d12813172e48be15d773bb1aace
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-08 23:23:23 +0530

    Added reverse of array

--- Array-ADT/08-Array.c
+++ Array-ADT/08-Array.c
@@ -19,11 +19,23 @@ void Reverse_aux(Array *arr)
     arr->arr[i] = B[i];
 }
 
+void Reverse(Array *arr)
+{
+  /* 
+    In this method we swap the index element with (length - index) element
+  */
+  for (int i = 0, j = arr->length - 1; i < j; i++, j--)
+  {
+    swap(&arr->arr[i], &arr->arr[j]);
+  }
+}
+
 int main()
 {
   Array arr = {{2, 3, 4, 5, 6}, 10, 5};
 
   Reverse_aux(&arr);
+  Reverse(&arr);
   Display(arr);
   return 0;
 }
