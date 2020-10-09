4a25479b3e2908006e9bc52d4ef9ea8c0f6d700f
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-09 15:47:54 +0530

    Completed C menu
    Added constructor and destructor in cpp
    Added Display function in cpp

--- Array-ADT/Cpp/main.cpp
+++ Array-ADT/Cpp/main.cpp
@@ -6,11 +6,35 @@ private:
   int *arr;
   int size;
   int length;
-  void swap(int& x, int& y);
+  void swap(int &x, int &y);
+
+  Array()
+  {
+  }
+  ~Array()
+  {
+    delete[] arr;
+  }
 
 public:
   void Display();
   void Append(int val);
   void Insert(int index, int val);
-  void LinearSearch()
+  int LinearSearch();
+  int BinarySearch();
+  int Get();
+  int Set();
+  int Max();
+  int Min();
+  int Sum();
+  float Avg();
+};
+
+void Array::Display()
+{
+  printf("Elements are\n");
+  for (int i = 0; i < length; i++)
+  {
+    printf("%d ", arr[i]);
+  }
 }
