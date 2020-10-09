e7399dcd3f87003a67ebcbc2b913befeb3614840
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-09 13:47:51 +0530

    creating difference of arrays

--- Array-ADT/14-Array.c
+++ Array-ADT/14-Array.c
@@ -1,24 +1,26 @@
 /* 
-  Intersection of 2 sorted arrays
+  Difference of arr2 from arr1 i.e arr1-arr2
+  both the arrays are sorted
 */
 
 #include <stdio.h>
 #include <stdlib.h>
 #include "Array.h"
 
-Array *Intersection_for_sorted(Array *arr1, Array *arr2)
+Array *Union_for_sorted(Array *arr1, Array *arr2)
 {
   int i = 0, j = 0, k = 0;
   Array *arr3 = (Array *)malloc(sizeof(Array));
   while (i < arr1->length && j < arr2->length)
   {
     if (arr1->arr[i] < arr2->arr[j])
-      i++;
+      arr3->arr[k++] = arr1->arr[i++]; //Copy the element of arr1
     else if (arr2->arr[j] < arr1->arr[i])
-      j++;
-    else //Copying element to array if equal
+      j++; //Skip the element in arr2
+    else
     {
-      arr3->arr[k++] = arr1->arr[i++];
+      // Skip the element if its equal
+      i++;
       j++;
     }
   }
@@ -35,7 +37,7 @@ int main()
   Array arr2 = {{3, 6, 7, 15, 20}, 10, 5};
   Array *arr3;
 
-  arr3 = Intersection_for_sorted(&arr1, &arr2);
+  arr3 = Union_for_sorted(&arr1, &arr2);
 
   Display(*arr3);
 
