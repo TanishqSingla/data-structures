861e14ada46a0af40e2900b4d3463d3291f0f5eb
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-09 14:55:14 +0530

    Added Difference of 2 arrays

--- Array-ADT/14-Array.c
+++ Array-ADT/14-Array.c
@@ -33,15 +33,46 @@ Array *Difference_for_sorted(Array *arr1, Array *arr2)
   return arr3;
 }
 
+Array *Difference(Array *arr1, Array *arr2)
+{
+  int k = 0;
+  Array *arr3 = (Array *)malloc(sizeof(Array));
+
+  int flag;
+
+  for (int i = 0; i < arr1->length; i++)
+  {
+    flag = 0;
+    for (int j = 0; j < arr2->length; j++)
+    {
+      if (arr1->arr[i] == arr2->arr[j])
+      {
+        flag = 1;
+        break;
+      }
+    }
+    if (flag == 0)
+      arr3->arr[k++] = arr1->arr[i];
+  }
+
+  arr3->length = k;
+  arr3->size = 10;
+
+  return arr3;
+}
+
 int main()
 {
   Array arr1 = {{2, 6, 10, 15, 25}, 10, 5};
   Array arr2 = {{3, 6, 7, 15, 20}, 10, 5};
   Array *arr3;
+  Array *arr4;
 
   arr3 = Difference_for_sorted(&arr1, &arr2);
+  arr4 = Difference(&arr1, &arr2);
 
   Display(*arr3);
+  Display(*arr4);
 
   return 0;
 }
