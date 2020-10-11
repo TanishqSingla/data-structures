0afc4b138463505b60c1683b395803f82a607491
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-11 21:51:13 +0530

    Adding constructor and functions to array class

--- Array-ADT/Cpp/main.cpp
+++ Array-ADT/Cpp/main.cpp
@@ -6,10 +6,18 @@ private:
   int *arr;
   int size;
   int length;
-  void swap(int &x, int &y);
-
-  Array()
+  void swap(int &x, int &y)
   {
+    int temp = x;
+    x = y;
+    y = temp;
+  }
+
+  Array(int s, int l)
+  {
+    size = s;
+    length = l;
+    arr = new int[size];
   }
   ~Array()
   {
@@ -17,9 +25,27 @@ private:
   }
 
 public:
-  void Display();
-  void Append(int val);
-  void Insert(int index, int val);
+  void Display()
+  {
+    std::cout << "Elements are" << std::endl;
+    for (int i = 0; i < length; i++)
+      std::cout << arr[i] << " ";
+  }
+  void Append(int val)
+  {
+    if (length < size)
+      arr[length++] = val;
+  }
+  void Insert(int index, int val)
+  {
+    if (index >= 0 && index <= length)
+    {
+      for (int i = length; i > index; i--)
+        arr[i] = arr[i - 1];
+      arr[index] = val;
+      length++;
+    }
+  }
   int LinearSearch();
   int BinarySearch();
   int Get();
@@ -29,12 +55,3 @@ public:
   int Sum();
   float Avg();
 };
-
-void Array::Display()
-{
-  printf("Elements are\n");
-  for (int i = 0; i < length; i++)
-  {
-    printf("%d ", arr[i]);
-  }
-}
