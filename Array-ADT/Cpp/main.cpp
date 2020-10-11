6b76af1398eea02fc5847fb7887940e907754d23
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-11 21:54:21 +0530

    Added delete and linear search

--- Array-ADT/Cpp/main.cpp
+++ Array-ADT/Cpp/main.cpp
@@ -46,7 +46,32 @@ public:
       length++;
     }
   }
-  int LinearSearch();
+  int Delete(int index)
+  {
+    int x = 0;
+
+    if (index >= 0 && index <= length - 1)
+    {
+      x = arr[index];
+      for (int i = index; i < length - 1; i++)
+        arr[i] = arr[i + 1];
+      length--;
+      return x;
+    }
+
+    return 0;
+  }
+  int LinearSearch(int key)
+  {
+    for (int index = 0; index < length; index++)
+    {
+      if (key == arr[index])
+        return index;
+    }
+
+    //Returning -1 if the element is not found
+    return -1;
+  }
   int BinarySearch();
   int Get();
   int Set();
