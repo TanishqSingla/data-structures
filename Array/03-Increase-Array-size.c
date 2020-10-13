5f28acdbe8d06546511507e256d118ba17bcb8d8
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-14 02:05:21 +0530

    Added increase array size

--- Array/03-Increase-Array-size.c
+++ Array/03-Increase-Array-size.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
+  int *p = (int *)malloc(sizeof(int) * 5);
+
+  //Initializing array
+  p[0] = 1, p[1] = 2, p[2] = 3, p[3] = 4, p[4] = 5;
+
+  int *q = (int *)malloc(sizeof(int) * 5);
+
+  //copying elements of p to q
+  for (int i = 0; i < 5; i++)
+    q[i] = p[i];
+
+  free(p);
+
+  p = q;
+  q = NULL;
+
   return 0;
 }
