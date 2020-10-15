da6d08e12fa6393e069ef061198acabf3a580cbc
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-16 01:53:46 +0530

    Added duplicates using strings

--- Strings/Duplicates-using-bits.c
+++ Strings/Duplicates-using-bits.c
@@ -2,6 +2,19 @@
 
 int main()
 {
+  char a[] = "finding";
+
+  long int H = 0, x = 0;
+
+  for (int i = 0; a[i] != '\0'; i++)
+  {
+    x = 1;
+    x <<= a[i] - 97;
+    if (x & H > 0)
+      printf("%c is duplicate", a[i]);
+    else
+      H = x | H;
+  }
 
   return 0;
 }
