66dc2ba9ef2721d8633440fed5e288e33f8ce425
TanishqSingla <tanishqsingla00@gmail.com>
2020-10-24 00:56:55 +0530

    Added count

--- Linked-Lists/04-Linked-list.c
+++ Linked-Lists/04-Linked-list.c
@@ -1,13 +1,30 @@
 #include "linkedList.h"
 
-int count()
+int count(struct Node *p)
 {
+  int count = 0;
+  while (p)
+  {
+    count++;
+    p = p->next;
+  }
+  return count;
+}
+
+int count_r(struct Node *p)
+{
+  if (!p)
+    return 0;
+  return count_r(p->next) + 1;
 }
 
 int main()
 {
   int arr[] = {1, 2, 3, 4, 5};
+
   create(arr, 5);
 
+  printf("Sum is %d\n", count(first));
+  printf("Sum is %d", count_r(first));
   return 0;
 }
