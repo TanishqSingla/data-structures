ee885162f268f53d31552fdd937ac7d42b9ff6d0
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-06 02:05:55 +0530

    Added linear search

--- Linked-Lists/07-Linked-list.c
+++ Linked-Lists/07-Linked-list.c
@@ -3,10 +3,42 @@
 
 #define Node struct Node
 
-void search(Node *p, int key);
+Node *search(Node *p, int key)
+{
+    while (p)
+    {
+        if (key == p->data)
+            return p;
+        p = p->next;
+    }
+    return NULL;
+}
+
+Node *search_r(Node *p, int key)
+{
+    if (p == NULL)
+        return NULL;
+    if (key == p->data)
+        return p;
+    return search_r(p->next, key);
+}
 
 int main()
 {
+    int arr[] = {1, 2, 3, 4, 5};
+
+    create(arr, 5);
+
+    Node *temp = search(first, 3);
+
+    if (temp)
+    {
+        printf("Key is found %d", temp->data);
+    }
+    else
+    {
+        printf("Key is not found");
+    }
 
     return 0;
 }
