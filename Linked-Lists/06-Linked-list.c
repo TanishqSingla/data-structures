addb5bd86c747d5d7599904cb29dee56acb84d6c
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-06 01:11:15 +0530

    Added maximum of all elemnents

--- Linked-Lists/06-Linked-list.c
+++ Linked-Lists/06-Linked-list.c
@@ -5,11 +5,26 @@
 
 int max(Node *p)
 {
-    int m = INT_MIN;
+    int m = p->data;
+
+    while (p)
+    {
+        if (p->data > m)
+            m = p->data;
+        else
+            p = p->next;
+    }
+    return m;
 }
 
 int main()
 {
+
+    int arr[] = {1, 2, 67, 4, 5};
+
+    create(arr, 5);
+
+    printf("Maximum element is %d", max(first));
 
     return 0;
 }
