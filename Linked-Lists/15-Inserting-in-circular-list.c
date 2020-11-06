de537bd8b44f3460bc68258422f6c022e88ce378
TanishqSingla <tanishqsingla00@gmail.com>
2020-11-07 03:30:59 +0530

    Adding inserting in a circular linked list

---
+++ Linked-Lists/15-Inserting-in-circular-list.c
@@ -0,0 +1,39 @@
+#include "circularList.h"
+
+void insert(Node *p, int index, int val)
+{
+    Node *temp;
+
+    if (index == 0)
+    {
+        temp = (Node *)malloc(sizeof(Node));
+        temp->data = val;
+        if (Head == NULL) // If there is no element in the linked list
+        {
+            Head = temp;
+            Head->next = Head;
+        }
+        else
+        {
+            while (p->next != Head)
+                p = p->next;
+            p->next = temp;
+            temp->next = Head;
+            Head = temp;
+        }
+    }
+    else
+    {
+        for (int i = 0; i < index; i++)
+            p = p->next;
+        temp = (Node *)malloc(sizeof(Node));
+        temp->data = val;
+
+        temp->next = p->next;
+        p->next = temp;
+    }
+}
+
+int main()
+{
+}
