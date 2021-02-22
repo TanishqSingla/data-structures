ab109b88af0a6b3f3bbc35c1cde3f965577c16a5
TanishqSingla <tanishqsingla00@gmail.com>
2021-02-22 23:16:55 +0530

    Added stack using array

--- Stack/01-Stack-using-array.c
+++ Stack/01-Stack-using-array.c
@@ -1,19 +1,65 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct stack
+struct Stack
 {
     int size;
     int top;
     int *s;
 };
 
+void create(struct Stack *st)
+{
+    printf("Enter size ");
+    scanf("%d", &st->size);
+    st->top = -1;
+    st->s = (int *)malloc(st->size * sizeof(int));
+}
+
+void display(struct Stack st)
+{
+    for (int i = st.top; i >= 0; i--)
+        printf("%d ", st.s[i]);
+    printf("\n");
+}
+
+void push(struct Stack *st, int val)
+{
+    if (st->top == st->size - 1)
+        printf("Stack Overflow\n");
+    else
+    {
+        st->top++;
+        st->s[st->top] = val;
+    }
+}
+
+int pop(struct Stack *st)
+{
+    int x = -1;
+    if (st->top == -1)
+    {
+        printf("Stack Underflow");
+    }
+    else
+    {
+        x = st->s[st->top--];
+    }
+    return x;
+}
+
 int main()
 {
-    struct stack st;
-    printf("Enter size of stack ");
-    scanf("%d", &st.size);
-    st.s = (struct stack *)malloc(sizeof(int) * st.size);
-    st.top = -1;
+    struct Stack st;
+    create(&st);
+    push(&st, 10);
+    push(&st, 20);
+    push(&st, 30);
+    display(st);
+
+    printf("%d\n", pop(&st));
+
+    display(st);
+
     return 0;
 }
