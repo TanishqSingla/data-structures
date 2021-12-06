ea21cd61df6a6094aa74834507e54f237e5c5b55
TanishqSingla <tanishqsingla00@gmail.com>
2021-12-07 03:31:21 +0530

    Adding push and pop function

--- Stack/02-Stack-using-linkedList.c
+++ Stack/02-Stack-using-linkedList.c
@@ -4,9 +4,25 @@
 struct Node {
     int data;
     struct Node* next;
-};
+}top = NULL;
+
+void push(int val) {
+    struct Node* temp;
+    temp = (struct Node*)malloc(sizeof(struct Node));
+    temp->data = val;
+    temp->next = NULL;
+}
+
+int pop() {
+    struct Node* temp;
+    if(top == NULL) {
+        printf("Stack is empty\n");
+    } else {
+        temp = top;
+        top = top->next;
+    }
+}
 
 int main() {
-    struct Node top = NULL;
     return 0;
 }
